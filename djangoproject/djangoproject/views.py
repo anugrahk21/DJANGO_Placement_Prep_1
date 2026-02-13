b3f1300fda17cdbd7b41ae3087b87b6be1dd3b98
ANUGRAH K <anugrah.k910@gmail.com>
2026-02-13 23:21:30 +0530

    Add student_data view to retrieve and render student list

--- djangoproject/djangoproject/views.py
+++ djangoproject/djangoproject/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from app.models import Student
+
 def home(request):
     return render(request,"index.html")
 
@@ -9,3 +11,7 @@ def login(request):
 
 def input_data(request):
     return render(request,"input.html")
+
+def student_data(request):
+    students = Student.objects.all()
+    return render(request,'student.html', {'students': students})
